5bbb8302c4a0b001e1048aaf31df597cf067da94
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-09-11 13:53:56 +0600

    database connection added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+// import require packages
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/db.js';
 
+// load environment variables from .env
 dotenv.config();
+
+// initialize application
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// database connection
+connectDB();
+
+// application routes
 app.get('/', (req, res) => {
   res.send('Hello from app');
 });
 
-app.listen(PORT, () => console.log(`🚀 Application running on http://localhost:${PORT}`));
+// start the application
+app.listen(PORT, () => console.log(`🚀 Application is running on http://localhost:${PORT}`));
